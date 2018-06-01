f03351a38ffec23bb509571aeb3ba48dd11068b4
AlbertMukhammadiev <albert.mukhammadiev@gmail.com>
2018-06-02 00:07:40 +0300

    fix function row_m for modify row algorithm and add tests for it

--- efficiency/migrations.py
+++ efficiency/migrations.py
@@ -1,5 +1,5 @@
 def cow(source, snapshot):
-    return 2 * sum(source * snapshot)
+    return sum(source * snapshot)
 
 
 def row(snapshots):
@@ -13,65 +13,33 @@ def row(snapshots):
     else:
         return 0
 
+
 def row_m(snapshots):
     if (len(snapshots) == snapshots.maxlen):
         removed = snapshots.popleft()
         migr = removed.copy()
-        current = snapshots.popleft()
-        while sum(migr):
-            next_snap = snapshots.popleft()
-            print(migr)
-            migr = (current != migr) * migr
-            print(migr, '\n')
-            next_migr = (next_snap != migr) * migr
-            current += (migr != next_migr)
-            snapshots.append(current)
-            current = next_snap
-            migr = next_migr
-        snapshots.append(current)
-        snapshots.appendleft(removed)
-
-def row_m1(snapshots):
-    if (len(snapshots) == snapshots.maxlen):
-        removed = snapshots.popleft()
-        current = snapshots.popleft()
-        migr = (current != removed) * removed
-        rest = (removed != migr)
-        while sum(migr):
+        current = removed.copy()
+        current.fill(False)
+        for i in range(snapshots.maxlen - 1):
             next_snap = snapshots.popleft()
             next_migr = (next_snap != migr) * migr
-            current += (migr != next_migr)
-            snapshots.append(current)
+            snapshots.append((migr != next_migr) + current)
             current = next_snap
             migr = next_migr
-        snapshots.append(current)
+            if not sum(migr):
+                snapshots.append(current)
+                #snapshots.maxlen - i + 1
+                snapshots.rotate(-snapshots.maxlen + i + 2)
+                break
+        else:
+            snapshots.append(current + migr)
+        
+        rest = snapshots.popleft()
         snapshots.appendleft(rest)
         return sum(removed) - sum(rest)
     else:
         return 0
 
-from collections import deque
-from numpy import array
 
 if __name__ == '__main__':
-    # arr0 = array([1,0,0,0,1,1,0,0,0,0,1,1]).astype(bool)
-    # arr1 = array([0,0,1,0,1,0,0,1,0,1,0,0]).astype(bool)
-    # arr2 = array([1,0,0,1,0,0,0,0,1,0,0,0]).astype(bool)
-    # arr3 = array([0,1,0,0,0,0,1,0,0,0,1,0]).astype(bool)
-    # arr4 = array([1,0,0,0,0,1,0,0,0,0,0,1]).astype(bool)
-    arr0 = array([1,0,0,0,1,1,0,0,0,0,1,1])
-    arr1 = array([0,0,1,0,1,0,0,1,0,1,0,0])
-    arr2 = array([1,0,0,1,0,0,0,0,1,0,0,0])
-    arr3 = array([0,1,0,0,0,0,1,0,0,0,1,0])
-    arr4 = array([1,0,0,0,0,1,0,0,0,0,0,1])
-    xs = deque([arr0, arr1, arr2, arr3, arr4], maxlen=5)
-    def print_snaps(snaps):
-        for snap in snaps:
-            for cell in snap:
-                print('*|', end='') if cell else print(' |', end='')
-            print()
-        print()
-
-    print_snaps(xs)
-    print(row_m1(xs))
-    print_snaps(xs)
+    pass
---
+++ tests/migrations.py
@@ -0,0 +1,212 @@
+from unittest import TestCase, main
+from collections import deque
+from numpy import array, array_equal
+from functools import partial
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from efficiency import migrations
+
+bool_array = partial(array, dtype=bool)
+
+class MigrationsTestCase(TestCase):
+    def setUp(self):
+        """initial set up"""
+        self.snapshots1 = deque(
+            [
+                bool_array([1,1,0,1,1,0,1,0]),
+                bool_array([0,1,0,0,0,0,0,0]),
+                bool_array([0,0,1,0,0,0,0,0]),
+                bool_array([0,0,0,0,0,0,0,0]),
+                bool_array([0,0,0,0,1,0,0,0]),
+                bool_array([0,0,0,0,0,1,0,0]),
+                bool_array([0,0,0,0,0,0,0,1])
+            ],
+            maxlen=7
+        )
+
+        self.snapshots2 = deque(
+            [
+                bool_array([1,1,1,1,1,1,0,0]),
+                bool_array([1,0,0,0,0,1,0,0]),
+                bool_array([0,1,0,0,1,0,0,0]),
+                bool_array([0,0,1,0,0,0,1,0]),
+                bool_array([0,0,0,1,0,0,0,1]),
+                bool_array([0,0,0,0,0,0,0,0]),
+                bool_array([0,0,1,1,0,0,0,0])
+            ],
+            maxlen=7
+        )
+
+        self.snapshots3 = deque(
+            [
+                bool_array([1,0,0,0,0,0,0,1]),
+                bool_array([0,1,0,0,0,0,0,0]),
+                bool_array([0,0,1,0,0,0,0,0]),
+                bool_array([0,0,0,1,0,0,0,0]),
+                bool_array([0,0,0,0,1,0,0,0]),
+                bool_array([0,0,0,0,0,1,0,0]),
+                bool_array([0,0,0,0,0,0,1,0])
+            ],
+            maxlen=7
+        )
+
+        self.snapshots4 = deque(
+            [
+                bool_array([1,0,1,0,0,0,0,1]),
+                bool_array([0,0,1,1,0,1,0,0]),
+                bool_array([0,1,0,0,1,0,1,0])
+            ],
+            maxlen=4
+        )
+
+        self.snapshots5 = deque(
+            [
+                bool_array([0,0,0,0,0,0,0,0]),
+                bool_array([0,1,0,1,0,0,0,1]),
+                bool_array([0,0,1,0,1,0,1,0]),
+                bool_array([1,0,0,0,0,1,0,0])
+            ],
+            maxlen=4
+        )
+
+        self.snapshots6 = deque(
+            [
+                bool_array([1,0,1,0,1,0,1,0]),
+                bool_array([1,1,0,1,1,0,1,1]),
+                bool_array([0,0,1,0,0,0,0,0]),
+                bool_array([0,0,0,1,0,0,0,0]),
+                bool_array([0,0,0,0,1,0,0,0]),
+                bool_array([0,0,0,0,0,1,0,0]),
+                bool_array([0,0,0,0,0,0,1,0]),
+                bool_array([0,0,0,0,0,0,0,1]),
+                bool_array([1,0,0,0,0,0,0,0]),
+                bool_array([0,1,0,0,0,0,0,0]),
+                bool_array([0,0,1,0,0,0,0,0]),
+                bool_array([0,0,0,1,0,0,0,0]),
+                bool_array([0,0,0,0,1,0,0,0]),
+                bool_array([0,0,0,0,0,1,0,0]),
+                bool_array([0,0,0,0,0,0,1,0]),
+                bool_array([0,0,0,0,0,0,0,1])
+            ],
+            maxlen=16
+        )
+
+
+    def test_cow(self):
+        """Verify correctly counts the number of migrating subblocks"""
+        source_block = bool_array([1,0,0,1,1,0,0,1,0,1])
+        new_block = bool_array([0,0,1,0,1,1,0,0,1,1])
+        self.assertEqual(migrations.cow(source_block, new_block), 2)
+
+        source_block = bool_array([1,0,0,1,1,0,0,1,0,1])
+        new_block = bool_array([0,1,1,0,0,1,1,0,1,0])
+        self.assertEqual(migrations.cow(source_block, new_block), 0)
+
+        source_block = bool_array([0,0,0,0,0,0,0,0,0,0])
+        new_block = bool_array([0,0,1,0,1,1,0,0,1,1])
+        self.assertEqual(migrations.cow(source_block, new_block), 0)
+
+
+    def test_row(self):
+        """Verify"""
+        previous_state = list(self.snapshots1)
+        self.assertEqual(migrations.row(self.snapshots1), 4)
+        self.assertEqual(len(self.snapshots1), 7)
+        indices = [0, 3, 4, 6]
+        previous_state[0][indices] = False
+        previous_state[1][indices] = True
+        for snap1, snap2 in zip(previous_state, self.snapshots1):
+            self.assertTrue(array_equal(snap1, snap2))
+
+        previous_state = self.snapshots2.copy()
+        self.assertEqual(migrations.row(self.snapshots2), 4)
+        self.assertEqual(len(self.snapshots2), 7)
+        indices = [1, 2, 3, 4]
+        previous_state[0][indices] = False
+        previous_state[1][indices] = True
+        for snap1, snap2 in zip(previous_state, self.snapshots2):
+            self.assertTrue(array_equal(snap1, snap2))
+
+        previous_state = self.snapshots3.copy()
+        self.assertEqual(migrations.row(self.snapshots3), 2)
+        indices = [0, 7]
+        previous_state[0][indices] = False
+        previous_state[1][indices] = True
+        self.assertEqual(len(self.snapshots3), 7)
+        for snap1, snap2 in zip(previous_state, self.snapshots3):
+            self.assertTrue(array_equal(snap1, snap2))
+        
+        previous_state = self.snapshots4.copy()
+        self.assertEqual(migrations.row(self.snapshots4), 0)
+        self.assertEqual(len(self.snapshots4), 3)
+        for snap1, snap2 in zip(previous_state, self.snapshots4):
+            self.assertTrue(array_equal(snap1, snap2))
+
+        previous_state = self.snapshots5.copy()
+        self.assertEqual(migrations.row(self.snapshots5), 0)
+        self.assertEqual(len(self.snapshots5), 4)
+        for snap1, snap2 in zip(previous_state, self.snapshots5):
+            self.assertTrue(array_equal(snap1, snap2))
+
+
+    def test_row_m(self):
+        """Verify"""
+        def print_snapshot(blocks):
+            print()
+            for block in blocks:
+                for subblock in block:
+                    print('#', end='') if subblock else print('_', end='')
+                print()
+
+        previous_state = list(self.snapshots1)
+        self.assertEqual(migrations.row_m(self.snapshots1), 4)
+        self.assertEqual(len(self.snapshots1), 7)
+        previous_state[0][[0, 3, 4, 6]] = False
+        previous_state[3][4] = True
+        previous_state[6][[0, 3, 6]] = True
+        for snap1, snap2 in zip(previous_state, self.snapshots1):
+            self.assertTrue(array_equal(snap1, snap2))
+
+        previous_state = self.snapshots2.copy()
+        self.assertEqual(migrations.row_m(self.snapshots2), 4)
+        self.assertEqual(len(self.snapshots2), 7)
+        previous_state[0][[1, 2, 3, 4]] = False
+        previous_state[1][[1, 4]] = True
+        previous_state[2][2] = True
+        previous_state[3][3] = True
+        for snap1, snap2 in zip(previous_state, self.snapshots2):
+            self.assertTrue(array_equal(snap1, snap2))
+
+        previous_state = self.snapshots6.copy()
+        self.assertEqual(migrations.row_m(self.snapshots6), 1)
+        self.assertEqual(len(self.snapshots6), 16)
+        previous_state[0][2] = False
+        previous_state[1][2] = True
+        for snap1, snap2 in zip(previous_state, self.snapshots6):
+            self.assertTrue(array_equal(snap1, snap2))
+
+        previous_state = self.snapshots3.copy()
+        self.assertEqual(migrations.row_m(self.snapshots3), 2)
+        self.assertEqual(len(self.snapshots3), 7)
+        previous_state[0][[0, 7]] = False
+        previous_state[6][[0, 7]] = True
+        for snap1, snap2 in zip(previous_state, self.snapshots3):
+            self.assertTrue(array_equal(snap1, snap2))
+        
+        previous_state = self.snapshots4.copy()
+        self.assertEqual(migrations.row_m(self.snapshots4), 0)
+        self.assertEqual(len(self.snapshots4), 3)
+        for snap1, snap2 in zip(previous_state, self.snapshots4):
+            self.assertTrue(array_equal(snap1, snap2))
+
+        previous_state = self.snapshots5.copy()
+        self.assertEqual(migrations.row_m(self.snapshots5), 0)
+        self.assertEqual(len(self.snapshots5), 4)
+        for snap1, snap2 in zip(previous_state, self.snapshots5):
+            self.assertTrue(array_equal(snap1, snap2))
+
+
+if __name__ == '__main__':
+    main()
